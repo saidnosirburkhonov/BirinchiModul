f7efc5226f930506021fac56782181f2ef5384e2
Saidnosir Burkhonov <saidnosirburxonov.com>
2025-12-04 12:59:26 +0500

    Homework

---
+++ Projects/BirinchiDars/BirinchiDars/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BirinchiDars;
+
+
+
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Son kiriting: ");
+        int intValue = int.Parse(Console.ReadLine());
+        intValue += 10;
+        Console.WriteLine(intValue);
+        int Value = int.Parse(Console.ReadLine());
+    }
+}
+
+
+
+
+
